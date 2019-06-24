dd1021c1495126d5f8b306e6cdc41efcdba58a71
Chuck52 <wuhan_53@163.com>
2019-06-24 10:23:10 +0800

    error log

--- services/server.js
+++ services/server.js
@@ -95,8 +95,7 @@ const checkData = receive => {
       receive.socket.end(pack({ code: 0, data: s }));
       // receive.socket.close();
     }).catch(err => {
-      logger.error(err);
-      console.trace(err);
+      logger.error(buffer, err);
       let code = -1;
       if(err === 'invalid command') { code = 1; }
       receive.socket.end(pack({ code }));
